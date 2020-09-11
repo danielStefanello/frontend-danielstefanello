0cfd6e537ef1ddd205ec01c4f76aa1b6c917f650
Daniel Stefanello <danieldfst@hotmail.com>
2020-09-11 08:38:40 -0300

    Update to temporary deploy

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import { Container, Menu, Footer, Logo } from './styles';
 
@@ -7,13 +7,13 @@ import logo from '../../assets/logo.svg';
 import instagram from '../../assets/instagram.svg';
 import linkedin from '../../assets/linkedin.svg';
 import whatsapp from '../../assets/whatsapp.svg';
-import discord from '../../assets/discord.svg';
+// import discord from '../../assets/discord.svg';
 
 export default function Main() {
   return (
     <Container>
       <Menu>
-        <li>
+        {/* <li>
           <Link to="/jobs">Jobs</Link>
         </li>
         <li>-</li>
@@ -23,32 +23,38 @@ export default function Main() {
         <li>-</li>
         <li>
           <Link to="/about">About</Link>
-        </li>
+        </li> */}
       </Menu>
       <Logo>
         <img src={logo} alt="Daniel Stefanello" />
       </Logo>
       <Footer>
         <li>
+          <a
+            href="https://www.linkedin.com/in/danielstefanello/"
+            target="blank"
+          >
+            <img src={linkedin} alt="Linkedin" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://api.whatsapp.com/send?phone=5551998223861&text=Ol%C3%A1%2520visitei%2520seu%2520site%2520pessoal%2520e%2520gostaria..."
+            target="blank"
+          >
+            <img src={whatsapp} alt="Whatsapp" />
+          </a>
+        </li>
+        <li>
           <a href="https://www.instagram.com/ddstefanello/" target="blank">
             <img src={instagram} alt="Instagram" />
           </a>
         </li>
-        <li>
-          <a href="https://www.instagram.com/ddstefanello/" target="blank">
-            <img src={linkedin} alt="Instagram" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/ddstefanello/" target="blank">
-            <img src={whatsapp} alt="Instagram" />
-          </a>
-        </li>
-        <li>
+        {/* <li>
           <a href="https://www.instagram.com/ddstefanello/" target="blank">
             <img src={discord} alt="Instagram" />
           </a>
-        </li>
+        </li> */}
       </Footer>
     </Container>
   );
